427362a4f2f8bae5a5a731c1725109f7eb1cbe89
yyang23 <yyang24@ivytech.edu>
2022-12-01 13:29:16 -0500

    Add two folders and file(course.js)

---
+++ models/course.js
@@ -0,0 +1,18 @@
+const db = require("../db");
+
+// Create a model from the schema
+const Course = db.model("Course", {
+   name: {type: String, required: true },
+   dept_code: String,
+   title: { type: String, required: true },
+   section:{type: String, required: true },
+   credit_hrs: { type: Number, min: 1, max: 5 },
+   instructor: {type: String, required: true },   
+   students: [ String ],
+   is_open: { type: Boolean, default: true },
+   is_deleted:{ type: Boolean, default: false },
+   created_on: { type: Date, default: Date.now },
+   edit_on: { type: Date, default: Date.now } 
+});
+
+module.exports = Course;
