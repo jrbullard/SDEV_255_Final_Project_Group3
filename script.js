03d31469105b77a521cdb02a20561613004cd5ef
Dakota Rogers <drogers135@ivytech.edu>
2022-12-03 13:24:27 -0600

    ?

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function check(checked = true) {
-  const checkboxes = document.querySelectorAll('input[name="class"]');
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", (event) => {
+  let checkboxes = document.querySelectorAll('input[name="class"]:checked');
+  let values = [];
   checkboxes.forEach((checkbox) => {
-      checkbox.checked = checked;
-}};
+    values.push(checkbox.value);
+  });
+  alert(values);
+});
