3a4d79109f9a9c858f394a4530998bc4eae181c5
Dakota Rogers <djrogers2016@outlook.com>
2022-12-04 13:17:27 -0600

    adding some pages and buttons

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const addbtn = document.querySelector("#addbtn");
+btn.addEventListener("click", (event) => {
+  let checkboxes = document.querySelectorAll('input[name="class"]:checked');
+  let values = [];
+  checkboxes.forEach((checkbox) => {
+    values.push(checkbox.value);
+  });
+});
