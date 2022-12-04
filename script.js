0aa877263728791b3d3dc3a7d26f1d63a53c47f6
Dakota Rogers <djrogers2016@outlook.com>
2022-12-04 11:35:25 -0600

    i can confidently say i have no idea what i am doing :)

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", (event) => {
-  let checkboxes = document.querySelectorAll('input[name="class"]:checked');
-  let values = [];
-  checkboxes.forEach((checkbox) => {
-    values.push(checkbox.value);
-  });
-  alert(values);
-});
+
