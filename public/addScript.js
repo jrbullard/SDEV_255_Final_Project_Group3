cdb43950d4ac283e33c92cd5cd9956bb2202efb5
jrbullard <jbullard8@ivytech.edu>
2022-12-05 16:41:40 -0500

    updated addScript

--- public/addScript.js
+++ public/addScript.js
@@ -1,35 +1,28 @@
 addEventListener("DOMContentLoaded", async function() {
-    document.querySelector("#deleteBtn").addEventListener("click", deleteCourse);
+    document.querySelector("#addBtn").addEventListener("click", addCourse);
     getAllCourses();
  });
  
- // Load all courses into the drop-down list
- async function getAllCourses() {
-    const response = await fetch("/api/courses");
-    if (response.ok) {
-       const courses = await response.json();
-       let html = "";
-       for (let course of courses) {
-          html += `<option value="${course._id}">${course.name}</option>`; 
-       }
- 
-       document.querySelector("#courseDropDown").innerHTML = html;
-    }
- }
- 
- async function addCourse() {
-    // Get the course ID of the selected course
-    const courseId = document.querySelector("#courseDropDown option:checked").value;
- 
-    const response = await fetch("/api/courses/" + courseId, {
-       method: "POST"
-    });
- 
-    if (response.ok) {
-         // Successfully added course
-         getAllCourses();
-    }
-    else {
-       document.querySelector("#error").innerHTML = "Cannot add course.";
-    } 
- }
+async function addCourse() {
+   const course = {
+      courseName: document.querySelector("#courseName").value,
+      courseSection: document.querySelector("#courseSection").value,
+      courseTitle: document.querySelector("#courseTitle").value,
+      instructor: document.querySelector("#instructor").value,
+   };
+
+   const response = await fetch("api/courses", {
+      method:"POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(course)
+   });
+   if (response.ok){
+      const results = await response.json();
+      alert("Added course: " +  results._id);
+
+      document.querySelector("form").reset();
+   }
+   else {
+      document.querySelector("#error").innerHTML = "Cannot add course.";
+   }
+}
